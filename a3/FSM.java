be20fbb960177e5cba8f75260845cd59b4679ab8
Leon-Lam919 <leonlam34@gmail.com>
2023-03-02 10:50:54 -0600

    delete

--- a3/FSM.java
+++ a3/FSM.java
@@ -6,148 +6,59 @@ public class FSM {
     public static void checkStateA(String n) {
        
         if(n.length() == 0) {
-            System.out.println("String not accepted");
+            System.out.println(" ");
         }
         else {
             if(n.charAt(0) == 'a') {
+                System.out.print("1");
                 stateB(n.substring(1));
             }
-            else {
-                stateF(n.substring(1));
+            else if(n.charAt(0) != 'b'){
+                System.out.print("0");
+                stateB(n.substring(1));
             }
         }
     }
  
     // function to check the stateB
     public static void stateB(String n) {
-       
         if(n.length() == 0) {
-            System.out.println("String not accepted");
+            System.out.println(" ");
         }
-        else {
-           
-            if(n.charAt(0) == 'a') {
-                stateC(n.substring(1));
-            }
-            else {
-                System.out.println("String not accepted");
-            }
+        if(n.charAt(0) == 'b') {
+            System.out.print("1");
+            stateC(n.substring(1));
+        }else if(n.charAt(0) != 'b'){
+            System.out.print("0");
+            stateC(n.substring(1));
         }
     }
  
     // function to check the stateC
     public static void stateC(String n) {
-       
         if(n.length() == 0) {
-            System.out.println("String not accepted");
+            System.out.println(" ");
         }
-        else {
-            if(n.charAt(0) == 'a') {
-                stateD(n.substring(1));
-            }
-            else {
-                System.out.println("String not accepted");
-            }
+        if(n.charAt(0) == 'c') {
+            System.out.print("1");
+            stateD(n.substring(1));
+        }else if(n.charAt(0) != 'b'){
+            System.out.print("0");
+            stateD(n.substring(1));
         }
     }
  
     // function to check the stateD
     public static void stateD(String n) {
-       
         if(n.length() == 0) {
-            System.out.println("String not accepted");
+            System.out.println(" ");
         }
-        else {
-           
-            if(n.charAt(0) == 'a') {
-                stateE(n.substring(1));
-            }
-            else {
-                System.out.println("String not accepted");
-            }
-        }
-    }
- 
-    // function to check the stateE
-    public static void stateE(String n) {
-       
-        if(n.length() == 0) {
-            System.out.println("String accepted");
-        }
- 
-        else if(n.charAt(0) == 'a') {
-            stateE(n.substring(1));
-        }
-        else if(n.charAt(0) == 'b') {
-            stateF(n.substring(1));
-        }
-    }
- 
-    // function to check the stateF
-    public static void stateF(String n) {
-
-        if(n.length() == 0) {
-            System.out.println("String not accepted");
-        }
-        else {
-
-            if(n.charAt(0) == 'b') {
-                stateG(n.substring(1));
-            }
-            else {
-                System.out.println("String not accepted");
-            }
-        }
-    }
- 
-    // function to check the stateG
-    public static void stateG(String n) {
-
-        if(n.length() == 0) {
-            System.out.println("String not accepted");
-        }
-        else {
-
-            if(n.charAt(0) == 'b') {
-                stateH(n.substring(1));
-            }
-            else {
-                System.out.println("String not accepted");
-            }
-        }
-    }
- 
-    // function to check the stateH
-    public static void stateH(String n) {
-
-        if(n.length() == 0) {
-            System.out.println("String not accepted");
-        }
-        else {
-
-            if(n.charAt(0) == 'b') {
-                stateQ(n.substring(1));
-            }
-            else {
-                System.out.println("String not accepted");
-            }
-        }
-    }
- 
-    // function to check the stateQ
-    public static void stateQ(String n) {
-
-        if(n.length() == 0) {
-            System.out.println("String accepted");
-        }
-        else {
-
-            if(n.charAt(0) == 'b') {
-                stateQ(n.substring(1));
-            }
-            else if(n.charAt(0) == 'a') {
-                stateB(n.substring(1));
-            }
+        if(n.charAt(0) == 'd') {
+            System.out.print("1");
+            checkStateA(n.substring(1));
+        }else if(n.charAt(0) != 'b'){
+            System.out.print("0");
+            checkStateA(n.substring(1));
         }
     }
  
